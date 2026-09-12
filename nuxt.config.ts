46304a29418cc04739f47bb089a2bedd7d285ce4
Muhammad Ikhsan Morgan Nurfiantoro <tellmesomething.morgan@gmail.com>
2026-09-12 21:00:17 +0700

    Modify nuxt.config.ts for GitHub Pages
    Comment out nitro preset configuration and update base URL.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,12 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
   //...
 
-  nitro: {
-    preset: 'github-pages',
-  },
+  // nitro: {
+  //   preset: 'github-pages',
+  // },
   
   app: {
-    baseURL: '/payme/', 
+    baseURL: '/', 
   },
 
   build: {
